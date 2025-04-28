770ab6a0bea727ef035f2340e8f9491b8ca06119
Shubham More <shubmore1999@gmail.com>
2025-04-28 16:56:30 -0500

    working on recommender

--- recommender/recommender.py
+++ recommender/recommender.py
@@ -1,1 +1,27 @@
-# algorithm
+
+class Recommender:
+	def __init__():
+		pass
+
+	def run_search():
+		pass
+
+	
+
+
+gcloud sql connect focusfeed-db --user=root --password=gargilicous
+
+
+/focusfeed/get_top_feed_notes/{item}
+/focusfeed/get_top_feed_notes/{SpaceX}
+{
+	{
+		feed1
+	},
+	{
+		feed2
+	},
+	...
+
+}
+
